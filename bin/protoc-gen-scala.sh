460d8fccc6c20eebec5b2a80bf5b521b97f88c04
Jeff Plaisance <jeffplaisance@gmail.com>
2010-08-17 06:23:02 -0500

    moving the compiled jar to the root directory

--- bin/protoc-gen-scala.sh
+++ bin/protoc-gen-scala.sh
@@ -3,7 +3,7 @@ SCRIPT=`readlink -f $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 CLASSPATH=${SCALA_HOME}/lib/scala-library.jar
-for i in ${SCRIPTPATH}/../lib/*.jar
+for i in ${SCRIPTPATH}/../lib/*.jar ${SCRIPTPATH}/../*.jar
 do
   CLASSPATH=${CLASSPATH}:${i}
 done
